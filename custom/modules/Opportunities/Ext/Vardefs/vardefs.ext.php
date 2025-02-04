27380c7ddcf63cc69f1da61f0a60688e8cfa0231
Areeba Sattar <areebasattar23@gmail.com>
2025-02-04 16:01:47 +0500

    repair n rebuild after replacing company with pipeline in filename

--- custom/modules/Opportunities/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/Opportunities/Ext/Vardefs/vardefs.ext.php
@@ -2,11 +2,6 @@
  //WARNING: The contents of this file are auto-generated
 
 
-// created: 2025-02-03 18:39:46
-$dictionary['Pipeline']['fields']['pipeline_id_c']['inline_edit']='1';
-$dictionary['Pipeline']['fields']['pipeline_id_c']['labelValue']='pipeline id';
-
-
  // created: 2025-02-03 17:14:47
 $dictionary['Opportunity']['fields']['jjwg_maps_address_c']['inline_edit']=1;
 
@@ -26,4 +21,9 @@ $dictionary['Opportunity']['fields']['jjwg_maps_lat_c']['inline_edit']=1;
 $dictionary['Opportunity']['fields']['jjwg_maps_lng_c']['inline_edit']=1;
 
  
+
+// created: 2025-02-03 18:39:46
+$dictionary['Pipeline']['fields']['pipeline_id_c']['inline_edit']='1';
+$dictionary['Pipeline']['fields']['pipeline_id_c']['labelValue']='pipeline id';
+
 ?>
