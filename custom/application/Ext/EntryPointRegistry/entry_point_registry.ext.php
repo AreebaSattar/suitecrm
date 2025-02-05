603cbe084eabdb65febec6cff1c0e087f84b1def
Areeba Sattar <areebasattar23@gmail.com>
2025-02-05 17:53:20 +0500

    repair and rebuild after added file getAccountDetails.php

---
+++ custom/application/Ext/EntryPointRegistry/entry_point_registry.ext.php
@@ -0,0 +1,10 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+$entry_point_registry['getAccountDetails'] = array(
+    'file' => 'custom/getAccountDetails.php',
+    'auth' => false
+);
+
+?>
