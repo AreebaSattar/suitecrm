8a7cceafc9a4fcf4e1c7e2913a52720ff4b1d1d6
Areeba Sattar <areebasattar23@gmail.com>
2025-02-05 17:25:39 +0500

    repair n rebuild after auto population company id on selcting account name using JS code

--- custom/modules/Opportunities/views/view.edit.php
+++ custom/modules/Opportunities/views/view.edit.php
@@ -1,1 +1,27 @@
 <?php
+require_once('include/MVC/View/views/view.edit.php');
+
+class OpportunitiesViewEdit extends ViewEdit {
+    public function display() {
+        parent::display();
+        // Custom JavaScript to Auto-Fill company_id_c
+        echo '<script type="text/javascript">
+            $(document).ready(function() {
+                $("#account_name").change(function() {
+                    var accountId = $("#account_id").val(); // Get Account ID
+                    if (accountId) {
+                        $.ajax({
+                            url: "index.php?module=Accounts&action=retrieve&record=" + accountId,
+                            dataType: "json",
+                            success: function(data) {
+                                if (data && data.company_id_c) {
+                                    $("#company_id_c").val(data.company_id_c); // Auto-fill field
+                                }
+                            }
+                        });
+                    }
+                });
+            });
+        </script>';
+    }
+}
