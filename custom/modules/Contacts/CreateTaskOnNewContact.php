7d4109c8771433059fb2eb0d2f607e292ce3b19d
Areeba Sattar <areebasattar23@gmail.com>
2025-02-09 02:29:22 +0500

    Email Notification working fine on Task Creation

--- custom/modules/Contacts/CreateTaskOnNewContact.php
+++ custom/modules/Contacts/CreateTaskOnNewContact.php
@@ -41,6 +41,44 @@ class CreateTaskOnNewContact
             $task->save();
 
             $GLOBALS['log']->info("Task created and linked to Contact ID: " . $bean->id);
+            $this->sendEmailNotification($task);
+        }
+    }
+    function sendEmailNotification($task)
+    {
+        global $current_user, $timedate;
+
+        $assignedUser = BeanFactory::getBean('Users', $task->assigned_user_id);
+
+        if (empty($assignedUser->email1)) {
+            $GLOBALS['log']->info("No email found for assigned user.");
+            return;
+        }
+
+        $subject = "A Task has been assigned to you";
+        $emailBody = "Hi " . $assignedUser->first_name . " " . $assignedUser->last_name . ",<br><br>";
+        $emailBody .= "A task has been assigned to you.<br><br>";
+        $emailBody .= "<b>Subject:</b> " . $task->name . "<br>";
+        $emailBody .= "<b>Priority:</b> " . $task->priority . "<br>";
+        $emailBody .= "<b>Due Date:</b> " . $timedate->to_display_date_time($task->date_due) . "<br>";
+        $emailBody .= "<b>Status:</b> " . $task->status . "<br>";
+        $emailBody .= "<b>Description:</b> " . $task->description . "<br><br>";
+        $emailBody .= "You may <a href='" . $GLOBALS['sugar_config']['site_url'] . "/index.php?module=Tasks&action=DetailView&record=" . $task->id . "'>review this task</a>.";
+
+        require_once('include/SugarPHPMailer.php');
+        $mail = new SugarPHPMailer();
+        $mail->setMailerForSystem();
+        $mail->From = $current_user->email1 ?: "areeba@sage-teck.com"; // Fallback email
+        $mail->FromName = "CRM Notifications";
+        $mail->Subject = $subject;
+        $mail->Body = $emailBody;
+        $mail->isHTML(true);
+        $mail->addAddress($assignedUser->email1);
+
+        if (!$mail->send()) {
+            $GLOBALS['log']->fatal("Email failed: " . $mail->ErrorInfo);
+        } else {
+            $GLOBALS['log']->info("Email sent successfully to " . $assignedUser->email1);
         }
     }
 }
