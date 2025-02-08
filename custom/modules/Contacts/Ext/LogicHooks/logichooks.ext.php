c698f0d361deaf808f15ff862f108a1181b9b864
Areeba Sattar <areebasattar23@gmail.com>
2025-02-08 22:58:52 +0500

    repair n rebuild after Class file added CreateTaskOnNewContact.php

---
+++ custom/modules/Contacts/Ext/LogicHooks/logichooks.ext.php
@@ -0,0 +1,14 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+$hook_array['after_save'][] = array(
+    1,
+    'Create task if contact meets conditions',
+    'custom/modules/Contacts/CreateTaskOnNewContact.php',
+    'CreateTaskOnNewContact',
+    'newTaskOnContact',
+);
+
+
+?>
