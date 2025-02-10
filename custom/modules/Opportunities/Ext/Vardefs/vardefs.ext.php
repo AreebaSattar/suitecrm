326f5d06129debb530b7a5ca5139142b4c91cf66
Areeba Sattar <areebasattar23@gmail.com>
2025-02-10 13:03:45 +0500

    Repair n rebuild after Label pipeline ID naming convention changed

--- custom/modules/Opportunities/Ext/Vardefs/vardefs.ext.php
+++ custom/modules/Opportunities/Ext/Vardefs/vardefs.ext.php
@@ -49,6 +49,6 @@ $dictionary['Opportunity']['fields']['jjwg_maps_lng_c']['inline_edit']=1;
 
 // created: 2025-02-03 18:39:46
 $dictionary['Pipeline']['fields']['pipeline_id_c']['inline_edit']='1';
-$dictionary['Pipeline']['fields']['pipeline_id_c']['labelValue']='pipeline id';
+$dictionary['Pipeline']['fields']['pipeline_id_c']['labelValue']='Pipeline ID';
 
 ?>
