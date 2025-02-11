6386ac1e16366894157afc774b854021fbc0a08f
Areeba Sattar <areebasattar23@gmail.com>
2025-02-11 14:01:44 +0500

    Repair n Rebuild after Update Contact Decsription from Task Hook added

---
+++ custom/modules/Tasks/Ext/LogicHooks/logichooks.ext.php
@@ -0,0 +1,15 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+$hook_array['after_save'][] = array(
+    1,
+    'Update Contact Description on the Basis of Task Status',
+    'custom/modules/Tasks/UpdateContactDescFromTask.php',
+    'UpdateContactDescFromTask',
+    'updateContactDescription',
+);
+
+
+
+?>
