a8c6d6966a0ed581277df023ae5025e08ea76b6f
Areeba Sattar <areebasattar23@gmail.com>
2025-02-13 16:00:16 +0500

    lead source + status desc code rewritten made changes

--- custom/modules/Leads/customleads.js
+++ custom/modules/Leads/customleads.js
@@ -1,26 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function toggleStatusDescription() {
+
+    function toggleStatusDescription() {   //For Status Description
         var statusDropdown = document.getElementById("status");
         var statusWrapper = document.querySelector('[data-field="status_description"]');
         var statusDescriptionField=document.getElementById("status_description");
-        if (statusWrapper && statusDropdown && statusDescriptionField ) { //all must present
+        if (statusWrapper && statusDropdown && statusDescriptionField ) { //all must present conditon
             var statusValue = statusDropdown.value;
             if (statusValue === "Recycled" || statusValue === "Dead") {
-                statusWrapper.style.display = "";
-                addToValidate("EditView", "lead_source_description", "text", true, "Lead Source Description");
+                statusDescriptionField.value = ""; //empty
+                statusWrapper.style.visibility = "visible";
+                addToValidate("EditView", "status_description", "text", true, "Status Description");
             }
             else{
-                statusWrapper.style.display = "none";
-                removeFromValidate("EditView", "lead_source_description");
+                statusDescriptionField.value = "";
+                statusWrapper.style.visibility = "hidden";
+                removeFromValidate("EditView", "status_description");
             }
         }
     }
+    //For Lead Source Description
     function toggleLeadsourceDescription() {
         var leadSourceDropdown=document.getElementById("lead_source");
         var leadSourceDescriptionField=document.getElementById("lead_source_description");
         if (leadSourceDropdown && leadSourceDescriptionField ) {
             var leadSourceVal= leadSourceDropdown.value;
-            if (leadSourceVal === "Other") {}
+            if (leadSourceVal === "Other") {
+                leadSourceDescriptionField.value = "";
+                addToValidate("EditView", "lead_source_description", "text", true, "Lead Source Description");
+            }
+            else{
+                leadSourceDescriptionField.value = "";
+                removeFromValidate("EditView", "lead_source_description");
+            }
         }
     }
 
@@ -28,11 +39,11 @@ document.addEventListener("DOMContentLoaded", function() {
     var leadSourceDropdown=document.getElementById("lead_source");
 
     if(statusDropdown) {
-        statusDropdown.addEventListener("change", toggleStatusDescription);
+        statusDropdown.addEventListener("change", toggleStatusDescription); //on change
         toggleStatusDescription();
     }
     if(leadSourceDropdown) {
         leadSourceDropdown.addEventListener("change", toggleLeadsourceDescription);
-        toggleLeadsourceDescription();
+        toggleLeadsourceDescription();//on page loads
     }
 });
