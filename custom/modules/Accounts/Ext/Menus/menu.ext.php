ecae418228d18e36f248f2551b9913cffdaf85f7
Areeba Sattar <areebasattar23@gmail.com>
2025-02-21 17:16:05 +0500

    Random

--- custom/modules/Accounts/Ext/Menus/menu.ext.php
+++ custom/modules/Accounts/Ext/Menus/menu.ext.php
@@ -2,15 +2,13 @@
  //WARNING: The contents of this file are auto-generated
 
 
+
 if (ACLController::checkAccess('Accounts', 'list', true)) {
     $module_menu[] = array(
         "index.php?module=Accounts&action=CustomerPartnersAccounts",
         "Customer & Partner Accounts",
         "Accounts"
     );
-}
-
-if (ACLController::checkAccess('Accounts', 'list', true)) {
     $module_menu[] = array(
         "index.php?module=Accounts&action=OtherThanCustomerPartnersAccounts",
         "Other than Customer & Partner Accounts",
