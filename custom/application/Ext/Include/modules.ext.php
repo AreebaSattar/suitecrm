d2e87ca028a766e96a147a73b02fbab0317d1cec
Areeba Sattar <areebasattar23@gmail.com>
2025-02-23 23:08:23 +0500

    Added new module Brands using Module Builder

--- custom/application/Ext/Include/modules.ext.php
+++ custom/application/Ext/Include/modules.ext.php
@@ -3,6 +3,13 @@
 
  
  //WARNING: The contents of this file are auto-generated
+$beanList['new_Brands'] = 'new_Brands';
+$beanFiles['new_Brands'] = 'modules/new_Brands/new_Brands.php';
+$moduleList[] = 'new_Brands';
+
+
+ 
+ //WARNING: The contents of this file are auto-generated
 $beanList['st_time_management'] = 'st_time_management';
 $beanFiles['st_time_management'] = 'modules/st_time_management/st_time_management.php';
 $moduleList[] = 'st_time_management';
